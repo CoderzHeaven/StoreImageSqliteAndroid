5e8f5bcc0fc9f0419612a6081b330a9aae3531fa
CoderzHeaven <coderzheaven@gmail.com>
2017-01-09 08:42:13 +0530

    Initial commit

---
+++ app/src/main/java/com/coderzheaven/storeimage/MainActivity.java
@@ -0,0 +1,129 @@
+package com.coderzheaven.storeimage;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatImageView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int SELECT_PICTURE = 100;
+    private static final String TAG = "MainActivity";
+
+    CoordinatorLayout coordinatorLayout;
+    FloatingActionButton btnSelectImage;
+    AppCompatImageView imgView;
+
+    DBHelper dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Find the views...
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinatorLayout);
+        btnSelectImage = (FloatingActionButton) findViewById(R.id.btnSelectImage);
+        imgView = (AppCompatImageView) findViewById(R.id.imgView);
+
+        btnSelectImage.setOnClickListener(this);
+
+        // Create the Database helper object
+        dbHelper = new DBHelper(this);
+
+    }
+
+    // Show simple message using SnackBar
+    void showMessage(String message) {
+        Snackbar snackbar = Snackbar.make(coordinatorLayout, message, Snackbar.LENGTH_LONG);
+        snackbar.show();
+    }
+
+    // Choose an image from Gallery
+    void openImageChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE) {
+
+                Uri selectedImageUri = data.getData();
+
+                if (null != selectedImageUri) {
+
+                    // Saving to Database...
+                    if (saveImageInDB(selectedImageUri)) {
+                        showMessage("Image Saved in Database...");
+                        imgView.setImageURI(selectedImageUri);
+                    }
+
+                    // Reading from Database after 3 seconds just to show the message
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            if (loadImageFromDB()) {
+                                showMessage("Image Loaded from Database...");
+                            }
+                        }
+                    }, 3000);
+                }
+
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        openImageChooser();
+    }
+
+    // Save the
+    Boolean saveImageInDB(Uri selectedImageUri) {
+
+        try {
+            dbHelper.open();
+            InputStream iStream = getContentResolver().openInputStream(selectedImageUri);
+            byte[] inputData = Utils.getBytes(iStream);
+            dbHelper.insertImage(inputData);
+            dbHelper.close();
+            return true;
+        } catch (IOException ioe) {
+            Log.e(TAG, "<saveImageInDB> Error : " + ioe.getLocalizedMessage());
+            dbHelper.close();
+            return false;
+        }
+
+    }
+
+    Boolean loadImageFromDB() {
+        try {
+            dbHelper.open();
+            byte[] bytes = dbHelper.retreiveImageFromDB();
+            dbHelper.close();
+            // Show Image from DB in ImageView
+            imgView.setImageBitmap(Utils.getImage(bytes));
+            return true;
+        } catch (Exception e) {
+            Log.e(TAG, "<loadImageFromDB> Error : " + e.getLocalizedMessage());
+            dbHelper.close();
+            return false;
+        }
+    }
+}
